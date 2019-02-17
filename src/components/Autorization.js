5c30ae522d876a6c76a5f7b2df17ac6518da774a
sunprime <sunprime@ukr.net>
2019-02-17 15:33:30 +0200

    update style

--- src/components/Autorization.js
+++ src/components/Autorization.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import {Link, Redirect} from "react-router-dom"
+import {Link} from "react-router-dom"
 import { withCookies } from 'react-cookie'
 
 import '../style/autorization.css'
